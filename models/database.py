a149dbe329c8a29501f5fa6f59538c17dc24914f
Arely <arelyelias70@gmail.com>
2024-03-06 19:55:27 -0600

    Style fix

--- models/database.py
+++ models/database.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://postgres:postgres@0.0.0.0:5432/postgres', echo=True)
+engine = create_engine(
+    "postgresql://postgres:postgres@0.0.0.0:5432/postgres", echo=True
+)
 Session = sessionmaker(bind=engine)
 session = Session()
 
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from models import database
 
+
 class Users(database.Base):
     __tablename__ = "users"
 
